6383a71756094a5caa8954f89283a3bb245f07a1
Jordan Jay <jordanjay28@gmail.com>
2016-10-01 00:53:00 -0700

    Inital test for pushing stuff to the repository

---
+++ gittest.java
@@ -0,0 +1,10 @@
+// testing out git
+
+public class gittest{
+	
+	public static void main(String args[]){
+		System.out.println("Hello world");
+		System.out.println("added an additional println to the java program");
+	}
+
+}
